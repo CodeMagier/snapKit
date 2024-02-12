6a916140a982345d195de2c5b9b6d835c9881afb
CodeMagier <Ios23.git@gmail.com>
2024-02-12 19:32:33 +0600

    Initial Commit

---
+++ snapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snapKit
+//
+//  Created by 1 on 12/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
